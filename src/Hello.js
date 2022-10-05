853fcacac7713bbb4adef4d7fef90a5f720a4d8a
Micheleg27 <michele.garribba27@github.com>
2022-10-06 00:34:32 +0200

    ciao

--- src/Hello.js
+++ src/Hello.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-export default class Hello extends React.Component {
-    render(){
-        return (<div>
-            <h1>Hello World</h1>,
-            <p>What a beautiful day</p>
-            </div>  
-            )
-}
-}
+// export default class Hello extends React.Component {
+//     render(){
+//         return (<div>
+//             <h1>Hello World</h1>,
+//             <p>What a beautiful day</p>
+//             </div>  
+//             )
+// }
+// }
 
-// export default function Hello () {
-//     return (<div>
-//         <h1>Hello World</h1>,
-//         <p>What a beautiful day</p>
-//         </div>  
-//     )
+export default function Hello () {
+    return (<div>
+        <h1>Hello World</h1>,
+        <p>What a beautiful day</p>
+        </div>  
+    )
+}
