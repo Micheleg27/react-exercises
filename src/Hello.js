6c86da4618950c3ff3a29effc06832d7b19bd53e
Micheleg27 <michele.garribba27@github.com>
2022-10-06 23:17:16 +0200

    fix

--- src/Hello.js
+++ src/Hello.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Message from "./Message";
 
-// export default class Hello extends React.Component {
-//     render(){
-//         return <h1>Hello World</h1>;
-//     }
-// }
-
-export default function Hello () {
-    return ( <div>
+export default class Hello extends React.Component {
+    render(){
+        return ( <div>
                 <h1>Hello World</h1>
                 <Message />
             </div>
         )
+    }
 }
