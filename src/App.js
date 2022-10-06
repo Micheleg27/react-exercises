6f23272b653e7ee962d89897b70f2e9a406fb42e
Micheleg27 <michele.garribba27@github.com>
2022-10-06 23:23:48 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import Hello from "./Hello";
 import { Welcome } from "./Welcome";
 
-// export default class App extends React.Component {
-//     render(){
-//         return <div><Hello /></div>;
-//     }
-// }
-
-export default function App () {
-    return <div>
+export default class App extends React.Component {
+    render(){
+        return <div>
             <Hello />
             <Welcome name="Michele"/>
-        </div>;
+        </div>;;
+    }
 }
+
+// export default function App () {
+//     return <div>
+//             <Hello />
+//             <Welcome name="Michele"/>
+//         </div>;
+// }
