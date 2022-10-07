0d84be9107637ee4d910242854bc8180663459af
Micheleg27 <michele.garribba27@github.com>
2022-10-07 13:34:43 +0200

    cond-rend04

--- src/Welcome.js
+++ src/Welcome.js
@@ -4,12 +4,12 @@ import { Age } from "./Age";
 export class Welcome extends React.Component {
     static defaultProps = {
         name: 'guest'
-      }
+    }
 
-    render(){
+    render() {
         return (<div>
             <p>Welcome, {this.props.name}</p>
-            {this.props.age > 18 && this.props.age < 65 && <Age age={this.props.age} />}
-            </div>)
+            {this.props.age > 18 && this.props.age < 65 && this.props.name === "John" && <Age age={this.props.age} />}
+        </div>)
     }
 }
