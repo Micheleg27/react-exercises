e60c979bcebdac3ce02d775dbd57299813dba734
Micheleg27 <michele.garribba27@github.com>
2022-10-07 12:30:38 +0200

    ciao

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ export default class App extends React.Component {
     render(){
         return <div>
                 <Hello />
-<<<<<<< HEAD
-                <Welcome name={<strong>Michele</strong>}/>
-=======
                 <Welcome name={<strong>Michele</strong>} age={17}/>
->>>>>>> conditional-rendering-01
                 </div>;
     }
 }
