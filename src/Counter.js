99e12f98b008794750211b02e783b83bf3b243c1
Micheleg27 <michele.garribba27@github.com>
2022-10-10 00:01:31 +0200

    lifecycle-01

--- src/Counter.js
+++ src/Counter.js
@@ -3,9 +3,39 @@ import { CounterDisplay } from './CounterDisplay';
 
 export class Counter extends React.Component {
 
-    render() {
-        return <h1> <CounterDisplay initialValue={1} /> </h1>
+    state = {
+        count: this.props.initialValue,
     }
+
+    // constructor(props) {
+    //     super(props);
+
+    // CONSTRUCTOR IS USELESS
+
+    // }
+
+    render() {
+        return <h1> <CounterDisplay count={this.state.count}/> </h1>
+    }
+
+    componentDidMount(){
+
+        setInterval(() => {
+            this.setState((state) => {
+    
+                return {
+                    count: state.count + (this.props.incrementBy)
+                }
+            })
+        }, this.props.timeout)
+    }
+
+}
+
+Counter.defaultProps = {
+    initialValue: 0,
+    incrementBy: 1,
+    timeout: 1000,
 }
 
 // State02: Il parametro del SetState dev'essere una callback invece di un oggetto 
