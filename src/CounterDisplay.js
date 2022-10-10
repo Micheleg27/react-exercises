ec210642204cdb67485fa570333226e8347c4e04
Micheleg27 <michele.garribba27@github.com>
2022-10-10 14:04:53 +0200

    fixed

--- src/CounterDisplay.js
+++ src/CounterDisplay.js
@@ -2,8 +2,40 @@ import React from "react";
 
 export class CounterDisplay extends React.Component {
 
-    render() {
-        return <h1>Count: {this.props.count}</h1>
+    state = {
+        count: this.props.initialValue,
     }
 
+    constructor(props) {
+        super(props);
+
+
+        setInterval(() => {
+            this.setState((state) => {
+                // STATE05: 
+                // if(state.count > this.props.initialValue * 10){
+                //     state.count = this.props.initialValue;
+                // }
+
+                // STATE05:
+                // return {
+                //     count: state.count > this.props.initialValue * 10 ? state.count = this.props.initialValue : state.count + (this.props.incrementBy)
+                // }
+
+                return {
+                    count: state.count + (this.props.incrementBy)
+                }
+            })
+        }, this.props.timeout)
+    }
+
+    render() {
+        return <h1>Count: {this.state.count}</h1>
+    }
+}
+
+CounterDisplay.defaultProps = {
+    initialValue: 0,
+    incrementBy: 1,
+    timeout: 1000,
 }
