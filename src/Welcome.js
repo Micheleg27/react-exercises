7619f81c0a291b3289a24603c1cafccc260c7f1a
Micheleg27 <michele.garribba27@github.com>
2022-10-16 14:14:52 +0200

    styling-01

--- src/Welcome.js
+++ src/Welcome.js
@@ -7,8 +7,8 @@ export class Welcome extends React.Component {
     }
 
     render() {
-        return (<div>
-            <p>Welcome, {this.props.name}</p>
+        return (<div className="welcome">
+            <p>Welcome, {this.props.name} </p>
             {this.props.age > 18 
             ? <Age age={this.props.age} />
             : <p>You are very young</p>}
