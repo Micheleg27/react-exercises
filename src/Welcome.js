1aadd72184a42d9e3f5eaac2c20830e268920207
Micheleg27 <michele.garribba27@github.com>
2022-10-17 03:12:30 +0200

    functional-01

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,21 +1,6 @@
 import React from "react";
 import { Age } from "./Age";
 
-// export class Welcome extends React.Component {
-//     static defaultProps = {
-//         name: 'guest'
-//     }
-
-//     render() {
-//         return (<div className="welcome">
-//             <p>Welcome, {this.props.name} </p>
-//             {this.props.age > 18 
-//             ? <Age age={this.props.age} />
-//             : <p>You are very young</p>}
-//         </div>)
-//     }
-// }
-
 export function Welcome ({name, age}) {
     return <div className="welcome">
         <p>Welcome, {name}</p>
