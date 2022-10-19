68dd3a4a44db5363384c8d4c30b0a3521a06e1b2
Micheleg27 <michele.garribba27@github.com>
2022-10-19 17:50:02 +0200

    fix

--- src/Sum.js
+++ src/Sum.js
@@ -11,6 +11,6 @@ Sum.defaultProps = {
 // Risolto con rest operator in quanto la traccia a mia interpretazione richiedeva
 // la somma a prescindere dal numero di elementi degli array, in altro caso:
 
-// export function Sum ({numbers: [num1, num2]}){
+// export function Sum ({numbers: [{num1 = 5}, {num2=6}]}){
 //     return <h1>The sum is: {num1 + num2}</h1>
 // }
