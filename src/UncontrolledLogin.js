a74c2a1450aeb9a965d620edf2feaca685e93c83
Micheleg27 <michele.garribba27@github.com>
2022-10-23 01:27:38 +0200

    useref2

--- src/UncontrolledLogin.js
+++ src/UncontrolledLogin.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export class UncontrolledLogin extends React.Component {
-    _formRef = React.createRef()
 
     formSubmit = (event) => {
         event.preventDefault()
