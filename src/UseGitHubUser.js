93779e0b983d46c6df5d40c550ddf165d04f548b
Micheleg27 <michele.garribba27@github.com>
2022-10-25 11:42:24 +0200

    fix custom hook 4

--- src/UseGitHubUser.js
+++ src/UseGitHubUser.js
@@ -2,21 +2,24 @@ import { useEffect, useState } from "react"
 
 export function useGitHubUser(username){
 
-    const [data, setData] = useState(null);
-    const [err, setError] = useState(null);
-    const [load, setLoad] = useState(false)
-
-    useEffect(() => {
+    function fetchDati(username){
         setLoad(true)
         fetch(`https://api.github.com/users/${username}`)
             .then( response => response.json())
             .then( json => setData(json))
             .catch((err) => setError(err))
             .finally (() => setLoad(false))
+    }
 
+    const [data, setData] = useState(null);
+    const [err, setError] = useState(null);
+    const [load, setLoad] = useState(false)
+
+    useEffect(() => {
+        fetchDati(username);
     }, [username])
 
     return (
-        {data, err, load, onGetUser: useGitHubUser}
+        {data, err, load, onGetUser: fetchDati}
     )
 }
