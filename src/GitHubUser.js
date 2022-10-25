6c3d6ad4010781c63aeb77aef683666e2fee4a85
Micheleg27 <michele.garribba27@github.com>
2022-10-25 18:48:49 +0200

    swr-03

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -2,9 +2,10 @@ import { useGitHubUser } from "./UseGitHubUser"
 
 export function GitHubUser ({username = 'gianmarcotoso'}){
 
-    const {data, load, err} = useGitHubUser(username)
+    const {data, load, err, onRefresh} = useGitHubUser(username)
 
     return (<div>
+        <button onClick={onRefresh}>Refresh</button>
         {data && <div>GitHub username is: {data.name}</div>}
         {err && <div>{err.message}</div>}
         {load && <div>Loading...</div>}
--- src/UseGitHubUser.js
+++ src/UseGitHubUser.js
@@ -1,17 +1,23 @@
 import useSWR from 'swr';
+import mutate from 'swr'
 
 export function useGitHubUser(username){
 
     const fetcher = (url) => fetch(url)
     .then((r) => r.json());
 
-    const {data, err} = useSWR(username ? `https://api.github.com/users/${username}` : null ,  fetcher )
+    const {data, err, mutate} = useSWR(username ? `https://api.github.com/users/${username}` : null ,  fetcher )
+
+    function refetchData(){
+        mutate();
+    }
 
     return (
         {
             data,
             err,
             load: !data && !err,
+            onRefresh: refetchData
         }
     )
 }
